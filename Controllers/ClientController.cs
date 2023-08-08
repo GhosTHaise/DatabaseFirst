c19181902bdb668bc3b291db23f5488b033bf340
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-08-08 20:23:57 +0300

    Create ClientController.cs

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseFirst.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClientController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<Database.Client>> Hello(){
+        using(var db = new DatabaseFirst.Database.BroadcastContext()){
+            List<DatabaseFirst.Database.Client> list = db.Clients.ToList();
+            return list;
+        }
+    }
+}
